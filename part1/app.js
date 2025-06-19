12cc6f00058ce633b6e15ed108d8c523c902bdc8
Machu <machu@machupicchu.com>
2025-06-19 12:05:23 +0000

    [automatic commit] 2025-06-19 12:05:23 UTC 1.147.68.158

--- part1/app.js
+++ part1/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const db = require('mysql2');
+const mysql = require('mysql2');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
